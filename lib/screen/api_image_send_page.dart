205d61833b78dd2005cc3e9b2eced7f83417df0d
HARSH-17177 <harsh3101gupta@gmail.com>
2022-12-17 14:47:41 +0530

    camera added

--- lib/screen/api_image_send_page.dart
+++ lib/screen/api_image_send_page.dart
@@ -46,7 +46,7 @@ class _SolutionState extends State<Solution> {
 
   Future getImage() async {
     final pickedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+        await ImagePicker().pickImage(source: ImageSource.camera);
     selectedImage = File(pickedImage!.path);
     setState(() {});
   }
